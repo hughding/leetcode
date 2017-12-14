737e886b7725566f684fdc6ca71a0fc033f7b7bc
HughDing <849308919@qq.com>
2017-12-14 21:53:29 +0800

    add RegularExpressionMatching

---
+++ src/main/java/pers/hugh/leetcode/RegularExpressionMatching.java
@@ -0,0 +1,31 @@
+package pers.hugh.leetcode;
+
+/**
+ * @author hugh
+ * @version 1.0
+ * @since <pre>2017/12/14</pre>
+ */
+public class RegularExpressionMatching {
+    //    10. Regular Expression Matching
+//    Implement regular expression matching with support for '.' and '*'.
+//
+//            '.' Matches any single character.
+//            '*' Matches zero or more of the preceding element.
+//
+//    The matching should cover the entire input string (not partial).
+//
+//    The function prototype should be:
+//    bool isMatch(const char *s, const char *p)
+//
+//    Some examples:
+//    isMatch("aa","a") → false
+//    isMatch("aa","aa") → true
+//    isMatch("aaa","aa") → false
+//    isMatch("aa", "a*") → true
+//    isMatch("aa", ".*") → true
+//    isMatch("ab", ".*") → true
+//    isMatch("aab", "c*a*b") → true
+    public boolean isMatch(String s, String p) {
+        return true;
+    }
+}
