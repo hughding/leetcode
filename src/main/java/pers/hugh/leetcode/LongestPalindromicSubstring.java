7affa567430c2838533919fdf2bee36faf19a796
hughding <849308919@qq.com>
2020-08-26 11:40:16 +0800

    Longest Palindromic Substring dp

--- src/main/java/pers/hugh/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/pers/hugh/leetcode/LongestPalindromicSubstring.java
@@ -49,51 +49,67 @@ public class LongestPalindromicSubstring {
         return palindromeStr;
     }
 
-    //矩阵,有问题
+    //dp[i,j] = dp[i+1,j-1] && s[i] == s[j]
     public String longestPalindromeOpt(String s) {
-        if (s == null || "".equals(s)) {
-            return "";
+        int n = s.length();
+        if(n == 0){
+            return s;
         }
-        int len = s.length();
 
-        int resultStartIndex = 0;
-        int resultSubLen = 0;
-        int startIndex = 0;
-        int subLen = 0;
-        for (int i = 0; i < len; i++) {
-            if (s.charAt(i) == s.charAt(len - i - 1)) {
-                if (startIndex == 0) {
-                    startIndex = i;
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
                 }
-                subLen = i - startIndex + 1;
-                if (subLen > resultSubLen) {
-                    resultStartIndex = startIndex;
-                    resultSubLen = subLen;
+                if (i == 0 && j > 0) {
+                    if(j > 1){
+                        dp[i][j] = dp[1][j - 1] && s.charAt(0) == s.charAt(j);
+                    }else{
+                        dp[i][j] = s.charAt(i) == s.charAt(j);
+                    }
                 }
-            } else {
-                startIndex = 0;
-                subLen = 0;
+                if (i > 0 && j == 0) {
+                    dp[i][j] = false;
+                }
+                if (i > 0 && j > 0) {
+                    if (i + 1 <= j - 1) {
+                        dp[i][j] = dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j);
+                    } else if (i <= j) {
+                        dp[i][j] = s.charAt(i) == s.charAt(j);
+                    }else {
+                        dp[i][j] = false;
+                    }
+                }
             }
         }
 
-        if (resultSubLen > 0) {
-            return s.substring(resultStartIndex, resultSubLen);
-        } else {
-            return "";
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j >= i && dp[i][j] == true && j - i > end - start) {
+                    start = i;
+                    end = j;
+                }
+            }
         }
+
+        return s.substring(start, end + 1);
     }
 
     public static void main(String[] args) {
         LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
         System.out.println(longestPalindromicSubstring.longestPalindrome("babad"));
         System.out.println(longestPalindromicSubstring.longestPalindrome("cbbd"));
-        System.out.println(longestPalindromicSubstring.longestPalindrome("a"));
+        System.out.println(longestPalindromicSubstring.longestPalindrome("aa"));
 
 
         System.out.println(longestPalindromicSubstring.longestPalindrome("==============="));
 
         System.out.println(longestPalindromicSubstring.longestPalindromeOpt("babad"));
         System.out.println(longestPalindromicSubstring.longestPalindromeOpt("cbbd"));
-        System.out.println(longestPalindromicSubstring.longestPalindromeOpt("a"));
+        System.out.println(longestPalindromicSubstring.longestPalindromeOpt("aa"));
     }
 }
