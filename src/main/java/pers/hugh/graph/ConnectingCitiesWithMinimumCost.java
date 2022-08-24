f48842afb63593f0dce4211abfe55a2bf58a220e
hughding <849308919@qq.com>
2022-08-24 11:13:23 +0800

    1135 题「最低成本联通所有城市」 Prim算法 1584. Min Cost to Connect All Points  Prim算法

--- src/main/java/pers/hugh/graph/ConnectingCitiesWithMinimumCost.java
+++ src/main/java/pers/hugh/graph/ConnectingCitiesWithMinimumCost.java
@@ -1,7 +1,6 @@
 package pers.hugh.graph;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 /**
  * @author dingxiuzheng
@@ -10,10 +9,11 @@ public class ConnectingCitiesWithMinimumCost {
 
     //VIP 收费题目
     //1135 题「最低成本联通所有城市」
-    //Kruskal 算法（克鲁斯卡尔算法）= 树判定 + 权重排序
     //地图上有n个城市，以1-n编号。connections[i] = [city1, city2, cost]，将城市city1、city2连接的成本是cost。
     //计算连通所有城市最小成本， 如果无法连通所有城市，返回-1
 
+
+    //Kruskal 算法（克鲁斯卡尔算法）= 树判定 + 权重排序
     public int minimumCost(int n, int[][] connections) {
         Arrays.sort(connections, Comparator.comparingInt(a -> a[2]));
         UnionFind uf = new UnionFind(n);
@@ -73,9 +73,87 @@ public class ConnectingCitiesWithMinimumCost {
         }
     }
 
+    //Prim算法，切分
+    public int minimumCost2(int n, int[][] connections) {
+        List<int[]>[] graph = buildGraph(n, connections);
+        Prim prim = new Prim(graph);
+        return prim.getWeightSum();
+    }
+
+    private List<int[]>[] buildGraph(int n, int[][] connections) {
+        List<int[]>[] graph = new LinkedList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new LinkedList<>();
+        }
+        for (int[] connection : connections) {
+            int from = connection[0] - 1, to = connection[1] - 1, weight = connection[2];
+            graph[from].add(new int[]{from, to, weight});
+            graph[to].add(new int[]{to, from, weight});
+        }
+        return graph;
+    }
+
+    public static class Prim {
+
+        private List<int[]>[] graph;
+
+        private int weightSum;
+
+        private Queue<int[]> queue;
+
+        private boolean[] inMST;
+
+        public Prim(List<int[]>[] graph) {
+            this.graph = graph;
+            this.weightSum = 0;
+            this.queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+            this.inMST = new boolean[graph.length];
+
+            cut(0);
+            inMST[0] = true;
+            while (!queue.isEmpty()) {
+                int[] edge = queue.poll();
+                int to = edge[1], weight = edge[2];
+                if (!inMST[to]) {
+                    weightSum += weight;
+                    cut(to);
+                    inMST[to] = true;
+                }
+            }
+        }
+
+        private void cut(int s) {
+            for (int[] edge : graph[s]) {
+                int to = edge[1];
+                if (!inMST[to]) {
+                    queue.add(edge);
+                }
+            }
+        }
+
+        public int getWeightSum() {
+            if (allConnected()) {
+                return weightSum;
+            } else {
+                return -1;
+            }
+        }
+
+        public boolean allConnected() {
+            for (boolean in : inMST) {
+                if (!in) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
         ConnectingCitiesWithMinimumCost solution = new ConnectingCitiesWithMinimumCost();
         System.out.println(solution.minimumCost(3, new int[][]{{1, 2, 5}, {1, 3, 6}, {2, 3, 1}}));
         System.out.println(solution.minimumCost(3, new int[][]{{1, 2, 5}}));
+        System.out.println(solution.minimumCost2(3, new int[][]{{1, 2, 5}, {1, 3, 6}, {2, 3, 1}}));
+        System.out.println(solution.minimumCost2(3, new int[][]{{1, 2, 5}}));
     }
 }
--- src/main/java/pers/hugh/graph/MinCostToConnectAllPoints.java
+++ src/main/java/pers/hugh/graph/MinCostToConnectAllPoints.java
@@ -1,9 +1,6 @@
 package pers.hugh.graph;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author dingxiuzheng
@@ -12,8 +9,8 @@ public class MinCostToConnectAllPoints {
 
     //1584. Min Cost to Connect All Points
     //https://leetcode.com/problems/min-cost-to-connect-all-points/
-    //网页上写代码，本地无测试用例
 
+    //Kruskal 算法（克鲁斯卡尔算法）= 树判定 + 权重排序
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
         List<int[]> edges = new ArrayList<>();
@@ -72,5 +69,87 @@ public class MinCostToConnectAllPoints {
             parent[x] = findRoot(parent[x]);
             return parent[x];
         }
+    }
+
+    //Prim算法
+    public int minCostConnectPoints2(int[][] points) {
+        List<int[]>[] graph = buildGraph(points);
+        Prim prim = new Prim(graph);
+        return prim.getWeightSum();
+    }
+
+    private List<int[]>[] buildGraph(int[][] points) {
+        int n = points.length;
+        List<int[]>[] graph = new LinkedList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new LinkedList<>();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    graph[i].add(new int[]{i, j, distance(points[i], points[j])});
+                }
+            }
+        }
+        return graph;
+    }
+
+    public static class Prim {
+        private List<int[]>[] graph;
+        private Queue<int[]> queue;
+        private boolean[] inMST;
+        private int weightSum;
+
+        public Prim(List<int[]>[] graph) {
+            this.graph = graph;
+            this.queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+            this.inMST = new boolean[graph.length];
+            this.weightSum = 0;
+            cut(0);
+            inMST[0] = true;
+            while (!queue.isEmpty()) {
+                int[] edge = queue.poll();
+                int to = edge[1], weight = edge[2];
+                if (!inMST[to]) {
+                    cut(to);
+                    weightSum += weight;
+                    inMST[to] = true;
+                }
+            }
+        }
+
+        private void cut(int s) {
+            for (int[] edge : graph[s]) {
+                int to = edge[1];
+                if (!inMST[to]) {
+                    queue.offer(edge);
+                }
+            }
+        }
+
+        public int getWeightSum() {
+            if (allConnected()) {
+                return weightSum;
+            } else {
+                return -1;
+            }
+        }
+
+        public boolean allConnected() {
+            for (boolean in : inMST) {
+                if (!in) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        MinCostToConnectAllPoints solution = new MinCostToConnectAllPoints();
+        System.out.println(solution.minCostConnectPoints(new int[][]{{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}}));
+        System.out.println(solution.minCostConnectPoints(new int[][]{{3, 12}, {-2, 5}, {-4, 1}}));
+        System.out.println(solution.minCostConnectPoints2(new int[][]{{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}}));
+        System.out.println(solution.minCostConnectPoints2(new int[][]{{3, 12}, {-2, 5}, {-4, 1}}));
     }
 }
